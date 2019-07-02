c8363bfafdf3d3ca566cfd399ba762909cfb6805
daksenik <daksenik@gmail.com>
2019-07-02 23:06:50 +0200

    Fix spacing.

--- HeatApp/models/ApiClient.js
+++ HeatApp/models/ApiClient.js
@@ -3,20 +3,20 @@ export class ApiClient {
 
   HOST = 'https://heat-backend.herokuapp.com';
 
-	get(url) {
-		return fetch(this.HOST + url)
+  get(url) {
+    return fetch(this.HOST + url)
       .then(response => response.json());
-	}
+  }
 
-	post(url, body) {
-		return fetch(this.HOST + url, {
-  			method: 'POST',
-  			headers: {
-  				Accept: 'application/json',
-  				'Content-Type': 'application/json',
-  			},
-  			body: JSON.stringify(body),
-  		})
-      .then(response => response.json());
-	}
+  post(url, body) {
+    return fetch(this.HOST + url, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    })
+    .then(response => response.json());
+  }
 }
